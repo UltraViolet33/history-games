e789eca66891379166fd8a90c148bd5c6f7a5d42
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-05-01 19:44:24 +0200

    add image links for queens

--- src/data/queens.js
+++ src/data/queens.js
@@ -4,7 +4,7 @@ export const queens = {
       id: "1",
       name: "Clotilde",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Sainte_Clotilde.jpg/220px-Sainte_Clotilde.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/a/a1/Vitrail_Florac_010609_12.jpg",
       date: "475 – 545",
       description:
         "Clotilde fut l'épouse de Clovis Ier, le fondateur de la dynastie mérovingienne et le premier roi à se convertir au christianisme en 496, un événement considéré comme fondateur de la monarchie française.",
@@ -13,79 +13,62 @@ export const queens = {
       id: "2",
       name: "Aliénor d'Aquitaine",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Eleanor_of_Aquitaine_by_Jules_Joseph_Lefebvre.jpg/220px-Eleanor_of_Aquitaine_by_Jules_Joseph_Lefebvre.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/a/a1/Queen_Eleanor_%28Frederick_Sandys%2C_1858%29.jpg",
       date: "1122 – 1204",
       description:
         "Aliénor d'Aquitaine fut la reine de France en tant qu'épouse de Louis VII et plus tard reine consort d'Angleterre en tant qu'épouse d'Henri II. Elle était également une figure influente de son temps en tant que mécène et protectrice des arts et des lettres.",
     },
     3: {
       id: "3",
-      name: "Isabeau de Bavière",
-      imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Etienne_Louis_Barr%C3%A9_-_Isabeau_de_Bavi%C3%A8re_-_Louvre_RF_1906-11.jpg/220px-Etienne_Louis_Barr%C3%A9_-_Isabeau_de_Bavi%C3%A8re_-_Louvre_RF_1906-11.jpg",
-      date: "1370 – 1435",
-      description:
-        "Isabeau de Bavière fut l'épouse de Charles VI et la reine de France de 1385 à 1422. Elle est surtout connue pour son rôle controversé dans la guerre de Cent Ans, son soutien à la faction bourguignonne et pour avoir été accusée de folie.",
-    },
-    4: {
-      id: "4",
       name: "Anne de Bretagne",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Anne_de_Bretagne.jpg/220px-Anne_de_Bretagne.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/b/ba/BNF_-_Latin_9474_-_Jean_Bourdichon_-_Grandes_Heures_d%27Anne_de_Bretagne_-_f._3r_-_Anne_de_Bretagne_entre_trois_saintes_%28d%C3%A9tail%29.jpg",
       date: "1477 – 1514",
       description:
         "Anne de Bretagne fut la reine de France en tant qu'épouse de Charles VIII et plus tard en tant qu'épouse de Louis XII. Elle est surtout connue pour son rôle dans l'union de la Bretagne et de la France et pour son mécénat des arts et des lettres.",
     },
-    5: {
-      id: "5",
+    4: {
+      id: "4",
       name: "Catherine de Médicis",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Fran%C3%A7ois_Clouet_-_Portrait_of_Catherine_de%27_Medici_-_Google_Art_Project.jpg/440px-Fran%C3%A7ois_Clouet_-_Portrait_of_Catherine_de%27_Medici_-_Google_Art_Project.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/d/d3/KatharinavonMedici.jpg",
       date: "1547 – 1559",
       description:
         "Catherine de Médicis était l'épouse de Henri II et mère de trois rois de France. Elle a joué un rôle important dans les affaires de l'État, notamment pendant les guerres de religion qui ont secoué la France au XVIe siècle.",
     },
-    6: {
-      id: "6",
+    5: {
+      id: "5",
       name: "Marie de Médicis",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/1/1a/Peter_Paul_Rubens_-_Portrait_of_Marie_de%27_M%C3%A9dicis%2C_Queen_of_France%2C_wife_of_Henry_IV_of_France_-_Google_Art_Project.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/9/96/0_Marie_de_M%C3%A9dicis_-_Frans_Pourbus_le_Jeune_-_Louvre_%28INV1710%29_-_%282%29.JPG",
       date: "1600 – 1610",
       description:
         "Marie de Médicis, fille de François Ier de Médicis, a épousé Henri IV en 1600. Elle est devenue reine de France à la mort de son mari en 1610 et a joué un rôle politique important pendant la minorité de leur fils, Louis XIII. Elle est également connue pour avoir commandé la construction du Palais du Luxembourg à Paris.",
     },
-    7: {
-      id: "7",
+    6: {
+      id: "6",
       name: "Anne d'Autriche",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/8/8f/Anne_of_Austria_%28Portrait_by_Peter_Paul_Rubens%29.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/2/25/After_Rubens_-_Anne_of_Austria%2C_Queen_of_France_-_Louvre_INV_1794.jpg",
       date: "1601 – 1666",
       description:
         "Anne d'Autriche fut reine de France et épouse de Louis XIII. Elle est notamment connue pour avoir donné naissance à Louis XIV, après 23 ans de mariage. Anne d'Autriche a également joué un rôle politique important, en tant que régente pendant la minorité de son fils, et en œuvrant pour la paix lors de la Guerre de Trente Ans.",
     },
-    8: {
-      id: "8",
+    7: {
+      id: "7",
       name: "Marie-Thérèse d'Autriche",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/4/4b/Marie_Th%C3%A9r%C3%A8se_d%27Autriche.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/f/fc/Marie-Th%C3%A9r%C3%A8se_d%27Autriche%2C_reine_de_France.jpg",
       date: "1660 – 1683",
       description:
         "Marie-Thérèse d'Autriche fut la première épouse de Louis XIV et la mère de ses six enfants. Elle a été considérée comme une reine influente, encourageant les arts et les lettres et soutenant les artistes de son temps.",
     },
-    9: {
-      id: "9",
-      name: "Marie Leszczynska",
+
+    8: {
+      id: "8",
+      name: "Marie-Antoinette d'Autriche",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Marie_Leszczynska%2C_Queen_of_France%2C_wife_of_Louis_XV.jpg/440px-Marie_Leszczynska%2C_Queen_of_France%2C_wife_of_Louis_XV.jpg",
-      date: "1725 – 1768",
-      description:
-        "Marie Leszczynska était une princesse polonaise qui est devenue reine de France en épousant Louis XV en 1725. Elle était connue pour son humilité et sa piété, ainsi que pour son rôle important dans l'éducation de ses enfants, dont le futur roi Louis XVI.",
-    },
-    10: {
-      id: "10",
-      name: "Marie-Antoinette",
-      imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Marie_Antoinette_-_Studio_of_Louis-Michel_Van_Loo.jpg/280px-Marie_Antoinette_-_Studio_of_Louis-Michel_Van_Loo.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/3/3b/Marie_Antoinette_Adult4.jpg",
       date: "1755 – 1793",
       description:
         "Marie-Antoinette fut la dernière reine de France avant la Révolution française. Elle est surtout connue pour son extravagance et son implication dans les affaires politiques, qui ont contribué à sa chute et à sa mort sur l'échafaud en 1793.",
@@ -96,7 +79,7 @@ export const queens = {
     "column-1": {
       id: "column-1",
       title: "Les Principaux Rois de France",
-      kingsIds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      kingsIds: [1, 2, 3, 4, 5, 6, 7, 8],
     },
   },
   columnOrder: ["column-1"],
