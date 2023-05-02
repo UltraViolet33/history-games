99dade182c5076ce073195dec2f0ca88dd73f61f
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-05-02 21:07:11 +0200

    add image link to battles

--- src/data/battles.js
+++ src/data/battles.js
@@ -41,13 +41,14 @@ export const battles = {
         "« Philippe-Auguste à Bouvines », illustration publiée dans le supplément illustré du Petit Journal, 26 juillet 1914.",
       date: "27 juillet 1214",
       description:
-        "La bataille de Bouvines est un affrontement militaire majeur qui a eu lieu le 27 juillet 1214 près de la ville de Bouvines, en France. Elle oppose les armées du roi de France Philippe Auguste à une coalition composée notamment de l'empereur du Saint-Empire Romain Germanique, d'une partie des barons anglais et du comte de Flandre.",
+        "La bataille de Bouvines, remportée par les troupes françaises est un affrontement militaire majeur qui a eu lieu le 27 juillet 1214 près de la ville de Bouvines, en France. Elle oppose les armées du roi de France Philippe Auguste à une coalition composée notamment de l'empereur du Saint-Empire Romain Germanique, d'une partie des barons anglais et du comte de Flandre.",
     },
     5: {
       id: "5",
       name: "Bataille de Crécy",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/1/1e/Crecy_en_Ponthieu_August_26%2C_1346_%28A%29.JPG",
+        "https://upload.wikimedia.org/wikipedia/commons/2/24/Battle_of_crecy_froissart.jpg",
+      imgDesc: "La bataille de Crécy. Jean Froissart, Chroniques",
       date: "26 août 1346",
       description:
         "La bataille de Crécy est une victoire décisive des troupes anglaises du roi Édouard III sur l'armée française du roi Philippe VI lors de la guerre de Cent Ans. Elle marque l'avènement de l'archerie anglaise comme arme principale de la guerre et la fin de la chevalerie comme force militaire dominante.",
@@ -56,7 +57,9 @@ export const battles = {
       id: "6",
       name: "Bataille d'Azincourt",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Agincourt-painting.jpg/800px-Agincourt-painting.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/b/be/Schlacht_von_Azincourt.jpg",
+      imgDesc:
+        "Bataille d'Azincourt, miniature tirée de l’Abrégé de la Chronique d'Enguerrand de Monstrelet, xve siècle",
       date: "25 octobre 1415",
       description:
         "La bataille d'Azincourt est une bataille de la guerre de Cent Ans qui a eu lieu le 25 octobre 1415 entre les armées anglaise et française. Les Anglais, commandés par le roi Henri V, ont remporté une victoire écrasante sur les Français, malgré leur infériorité numérique. La bataille est devenue célèbre pour la charge héroïque des archers anglais et pour la capture de nombreux nobles français, dont le connétable de France, Charles d'Albret.",
@@ -65,7 +68,8 @@ export const battles = {
       id: "7",
       name: "Bataille de Marignan",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Pieter_Bruegel_der_Ältere_092.jpg/800px-Pieter_Bruegel_der_Ältere_092.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/1/1a/Marignano.jpg",
+      imgDesc: "Peinture du xvie siècle attribuée au Maître à la Ratière",
       date: "13-14 septembre 1515",
       description:
         "La bataille de Marignan est une bataille qui s'est déroulée le 13 et le 14 septembre 1515 près de la commune italienne de Marignan, au sud-est de Milan, entre les armées françaises de François Ier et les troupes suisses, alliées aux ducs de Milan et de Venise. La victoire française a permis à François Ier de consolider son influence en Italie.",
@@ -74,7 +78,9 @@ export const battles = {
       id: "8",
       name: "Bataille de Valmy",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Bataille_de_Valmy.jpg/280px-Bataille_de_Valmy.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/c/c8/Valmy_Battle_painting.jpg",
+      imgDesc:
+        "La bataille de Valmy. le 20 septembre 1792. Peinture d'Horace Vernet, 1826.",
       date: "20 septembre 1792",
       description:
         "La bataille de Valmy est considérée comme le premier affrontement majeur des guerres de la Révolution française. Les armées républicaine et prussienne se sont affrontées dans la plaine de Valmy, en Champagne. Les forces prussiennes étaient largement supérieures en nombre, mais l'armée républicaine, sous le commandement de Kellermann et Dumouriez, a réussi à les repousser après une journée de combats. Cette victoire a permis aux républicains de gagner du temps et de remporter une première victoire symbolique contre les puissances étrangères qui cherchaient à rétablir la monarchie en France.",
@@ -84,7 +90,8 @@ export const battles = {
       id: "9",
       name: "Bataille d'Austerlitz",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/9/9d/Austerlitz_bataille_2_decembre_1805.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/b/ba/La_bataille_d%27Austerlitz._2_decembre_1805_%28Fran%C3%A7ois_G%C3%A9rard%29.jpg",
+      imgDesc: "Napoléon à la bataille d'Austerlitz par François Gérard.",
       date: "2 décembre 1805",
       description:
         "La bataille d'Austerlitz est considérée comme la plus grande victoire militaire de Napoléon Bonaparte. Elle a opposé les troupes françaises aux armées austro-russe et a conduit à la défaite de ces dernières. La bataille est également connue sous le nom de bataille des Trois Empereurs.",
@@ -93,7 +100,9 @@ export const battles = {
       id: "10",
       name: "Bataille de Waterloo",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/1/1b/Bataille_de_Waterloo_1815.PNG",
+        "https://upload.wikimedia.org/wikipedia/commons/b/bb/Andrieux_-_La_bataille_de_Waterloo.jpg",
+      imgDesc:
+        "La Bataille de Waterloo. 18 juin 1815, par Clément-Auguste Andrieux, 1852.",
       date: "18 juin 1815",
       description:
         "La bataille de Waterloo est une bataille qui s'est déroulée le 18 juin 1815 près de Waterloo en Belgique, durant laquelle l'armée française dirigée par Napoléon Bonaparte a été défaite par les armées britannique et prussienne commandées respectivement par le duc de Wellington et le maréchal Blücher. Cette défaite marque la fin des guerres napoléoniennes et de la domination française en Europe.",
@@ -102,7 +111,9 @@ export const battles = {
       id: "11",
       name: "Bataille de Verdun",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Verdun_21-23_f%C3%A9vrier_1916-5.jpg/800px-Verdun_21-23_f%C3%A9vrier_1916-5.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/e/e6/Bataille_de_Verdun_1916.jpg",
+      imgDesc:
+        "Soldats français à l'assaut sortant de leur tranchée pendant la bataille de Verdun, 1916.",
       date: "21 février 1916 – 19 décembre 1916",
       description:
         "La bataille de Verdun a été l'une des plus importantes de la Première Guerre mondiale, opposant les armées française et allemande pendant près de dix mois en 1916. Elle a été marquée par une violence et une intensité inouïes, avec des pertes humaines estimées à plus de 700 000 soldats de part et d'autre.",
@@ -111,7 +122,9 @@ export const battles = {
       id: "12",
       name: "Bataille de France",
       imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/f/f5/Blitzkrieg_Tank.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/f/fa/Bundesarchiv_Bild_183-H28708%2C_Paris%2C_Eiffelturm%2C_Besuch_Adolf_Hitler.jpg",
+      imgDesc:
+        "Hitler, entouré de ministres et d‘officiers, sur l‘esplanade du Trocadéro, devant la tour Eiffel, le 23 juin 1940.",
       date: "10 mai 1940 - 25 juin 1940",
       description:
         "La Bataille de France, également appelée Campagne de France, est la phase finale de la guerre de l'Ouest de la Seconde Guerre mondiale. Elle oppose l'Allemagne nazie et ses alliés à la France et au Royaume-Uni, qui défendent leur territoire. La bataille se termine par une victoire écrasante de l'Allemagne, qui occupe ensuite la France pendant quatre ans.",
